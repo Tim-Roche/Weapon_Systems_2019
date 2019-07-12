8c0a8d07abaa5ee7d05ffd9d3b712b81ebdd49f5
Tim Roche Laptop <kc2vsa@gmail.com>
2019-07-12 19:08:47 -0400

    Posistion graph looking good for KF

--- Final_Project/kalmanFilter.m
+++ Final_Project/kalmanFilter.m
@@ -3,7 +3,7 @@
 %All Hail: http://www.ilectureonline.com/lectures/subject/SPECIAL%20TOPICS/26/190
 
 SIM_TIME = 50;
-dt = 2;
+dt = 0.01;
 TOTAL_POINTS = int64(SIM_TIME/dt) + 1;
 
 time = 0:dt:SIM_TIME;
@@ -12,22 +12,21 @@ time = 0:dt:SIM_TIME;
 x = zeros(2, TOTAL_POINTS);
 p = zeros(2,2,TOTAL_POINTS);
 
+%Measurement Errors
+R_true = 900;
+R = 0.95*R_true; 
+
 %Initial Condition
 P_0 = 0; %Initial Posistion
 V_0 = 200; %Initial Velocity
 x(:,1) = [P_0;V_0]; %x_prediction
-p(:,:,1) = [1, 1; 1, 1]; %Process Uncertanty
-
-%Measurement Errors
-R_true = 900;
-R = 0.95*R_true; 
+p(:,:,1) = [1, (1/dt); (1/dt), (2/dt)^2]*R_true; %Process Uncertanty
 
 %Matrixes
 A = [1, dt; 0, 1]; %Transistion Matrix
 C = [1,0;0,1];
 
 %Various Errors
-w = 0;
 Q = 0;
 
 %"Conversion" Matrix
@@ -50,29 +49,34 @@ for i = 2:TOTAL_POINTS
     %Get New Measured Value
     noiseX = normrnd(0, sqrt(R_true));
     measuredX = truth(1,i) + noiseX;%C*Y_m + z; 
-    previousX = x(1, i-1);
-    z = previousX;
+    z = measuredX;
 
     %Process Noise
     Q = [(1/4)*dt^4, (1/2)*dt^3; (1/2)*dt^3, dt^2]*W;
     
     %New Predicted State
-    x_pC = A*x(:, i-1) + w; %Assuming no acceleration or error
-    p_pC = A*p(:, :, i-1) + Q; %Assuming no error
+    x_pC = A*x(:, i-1); %Assuming no acceleration or error
+    p_pC = A*p(:, :, i-1)*A.' + Q; %Assuming no error
     
     %Gain Computation
-    KG = (p_pC*H.')/((H*p_pC*H.') + R);
+    KG = (p_pC*H.')/(H*p_pC*H.' + R);
     
     %Calculate Current State
     x(:, i) = x_pC + KG*(z - H*x_pC);
-    p(:, :, i) = (eye(2) - KG*H)*p_pC*(eye(2)-KG*H).'+KG*R*KG.';
+    p(:, :, i) = (eye(2) - KG*H)*p_pC; %(eye(2) - KG*H)*p_pC*(eye(2)-KG*H).'+KG*R*KG.';
 end
 
 figure(1);
 posP = zeros(1,TOTAL_POINTS); 
+deltaX = zeros(1, TOTAL_POINTS);
 for i = 1:TOTAL_POINTS
     posP(i) = p(1,1,i);
+    deltaX(i) = x(1,i) - truth(1, i);
 end
 
-threeSigmaAbove = 3*arrayfun(sqrt, posP);
-
+threeSigmaAbove = 3*sqrt(posP);
+hold on
+plot(time, threeSigmaAbove, 'LineWidth',2,  'Color','k');
+plot(time, -threeSigmaAbove, 'LineWidth',2, 'Color','k');
+plot(time, deltaX, 'Color', 'b');
+hold off
