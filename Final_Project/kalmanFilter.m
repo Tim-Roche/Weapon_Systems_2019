e9fe93cee261d6d8dda5313e0f6a4ec2a715c289
Tim Roche Laptop <kc2vsa@gmail.com>
2019-07-11 23:48:24 -0400

    :((((((

--- Final_Project/kalmanFilter.m
+++ Final_Project/kalmanFilter.m
@@ -3,32 +3,57 @@
 %All Hail: http://www.ilectureonline.com/lectures/subject/SPECIAL%20TOPICS/26/190
 
 SIM_TIME = 30;
-delta = 0.1;
+dt = 0.1;
 TOTAL_POINTS = 300;
-TM = [1;2]
 
 %Initial Condition
-x_p(1) = [10;10]; %x_prediction
-p_p(1) = [1; 1]; %Process Uncertanty
+P_0 = 0; %Initial Posistion
+V_0 = 200; %Initial Velocity
+x_p(1) = [P_0;V_0]; %x_prediction
+p_p(1) = [1; 1; 1; 1]; %Process Uncertanty
 
-A = [1, delta; 0, 1]; %Transistion Matrix
+%Measurement Errors
+R_true = 900;
+R = 0.95*R_true; 
 
+%Matrixes
+A = [1, dt; 0, 1]; %Transistion Matrix
+C = [1,0;0,1];
+
+%Various Errors
+z = [0;0]; %Error in the mechanism to make observ.
+w = 0;
+Q = 0;
+
+%"Conversion" Matrix
 H = [1,0;1,0];
-R = 0; %Yet to be defined
-C = [1,0;1,0];
-Z = [0;0]; %Error in the mechanism to make observ.
+
+%Noise
+W_true = 12;
+W = 0.9*W_true;
+
 for i = 2:TOTAL_POINTS
+    %Find true posistion
+    x_true(:, i) = A*x_true(:,i-1);
+    
+    %Garbage-ify the true posistion for my sensor to measurement
+    variance = var(Y_m);
+    Y_m(:,1) = [1;1]; %PLACEHOLDER!!!
+    
+    %Process Noise
+    Q = [(1/4)*dt^4, (1/2)*dt^3; (1/2)dt^3, dt^2]*W;
+    
     %New Predicted State
-    x_p(:,i) = A*x_p(:,i-1); %Assuming no acceleration or error
-    p_p(:,i) = A*p_p(:,i-1); %Assuming no error
+    x_pC = A*x(:, i-1) + w; %Assuming no acceleration or error
+    p_pC = A*p(:, :, i-1) + Q; %Assuming no error
     
     %Gain Computation
-    KG = (p_p(:,i)*H.')/((H*p_p(:,i)*H.') + R);
+    KG = (p_pC*H.')/((H*p_pC*H.') + R);
     
     %Get New Measured Value
-    Y = C*Y_m + Z; 
+    Y = C*Y_m + z; 
     
     %Calculate Current State
-    x = x_p(:,i) + KG*(Y - H*x_p(:,i));
-    p = (eye(2) - K*H)*p_p(:,i);
+    x(:, i) = x_pC + KG*(Y - H*x_pC);
+    p(:, :, i) = (eye(2) - K*H)*p_pC*(I-K*H).'+K*R*K.';
 end
