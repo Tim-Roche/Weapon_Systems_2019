58a4d1b15686d53181c2741f476ef3d79d35e5b8
Tim Roche Laptop <kc2vsa@gmail.com>
2019-07-12 21:31:12 -0400

    pre monte

--- Final_Project/kalmanFilter.m
+++ Final_Project/kalmanFilter.m
@@ -1,40 +1,54 @@
 %Timothy Roche
 %Final Project Weapon Systems
-%All Hail: http://www.ilectureonline.com/lectures/subject/SPECIAL%20TOPICS/26/190
 
 SIM_TIME = 50;
 dt = 2;
 TOTAL_POINTS = int64(SIM_TIME/dt) + 1;
 
+%Initial Condition
+P_0 = 0; %Initial Posistion
+V_0 = 200; %Initial Velocity
+
+figure(1);
+movegui('west');
+trueAccel = NaN; %Truth acceleration is white noise
+[x, p, truth, time] = kf(trueAccel, SIM_TIME, dt, P_0, V_0,TOTAL_POINTS);
+graphName = "1D Kalman Filter -- a = White Noise";
+plotToGraph(graphName, x, p, truth, time, TOTAL_POINTS);
+
+figure(2);
+movegui('east');
+trueAccel = 15; %Truth acceleration is 15m/s^2
+[x_a, p_a, truth_a, time_a] = kf(trueAccel, SIM_TIME, dt, P_0, V_0,TOTAL_POINTS);
+graphName_a = "1D Kalman Filter -- a = 15m/s^2";
+plotToGraph(graphName_a, x_a, p_a, truth_a, time_a, TOTAL_POINTS);
+
+%----------Functions-------------
+function [x, p, truth, time] = kf(acc, SIM_TIME, dt, P_0, V_0, TOTAL_POINTS)
 time = 0:dt:SIM_TIME;
 
 %Defining Key Matrixes
 x = zeros(2, TOTAL_POINTS);
 p = zeros(2,2,TOTAL_POINTS);
 
-%Measurement Errors
-R_true = 900;
-R = 0.95*R_true; 
-
-%Initial Condition
-P_0 = 0; %Initial Posistion
-V_0 = 200; %Initial Velocity
-x(:,1) = [P_0;V_0]; %x_prediction
-p(:,:,1) = [1, (1/dt); (1/dt), (2/dt)^2]*R_true; %Process Uncertanty
-
 %Matrixes
 A = [1, dt; 0, 1]; %Transistion Matrix
 C = [1,0;0,1];
 
-%Various Errors
-Q = 0;
-
 %"Conversion" Matrix
 H = [1,0];%;1,0];
+
+%Measurement Errors
+R_true = 900;
+R = 0.95*R_true; 
 
 %Noise
 W_true = 12;
 W = 0.9*W_true;
+
+%Initial Values
+x(:,1) = [P_0;V_0]; %x_prediction
+p(:,:,1) = [1, (1/dt); (1/dt), (2/dt)^2]*R_true; %Process Uncertanty
 
 truth = zeros(2, TOTAL_POINTS);
 truth(:, 1) = [P_0; V_0];
@@ -43,9 +57,10 @@ for i = 2:TOTAL_POINTS
     B = [(1/2)*dt^2; dt]; %Control Matrix for accel.
     
     %Find true posistion
-    noiseA = normrnd(0, sqrt(W_true));
-    truth(:,i) = A*truth(:,i-1) + B*noiseA;
-    
+    if(isnan(acc)) %Check to see if this is Part 1 or Part 1.1
+        acc = normrnd(0, sqrt(W_true)); %Make Acc white noise instead
+    end
+    truth(:,i) = A*truth(:,i-1) + B*acc;
     %Get New Measured Value
     noiseX = normrnd(0, sqrt(R_true));
     measuredX = truth(1,i) + noiseX;%C*Y_m + z; 
@@ -65,29 +80,55 @@ for i = 2:TOTAL_POINTS
     x(:, i) = x_pC + KG*(z - H*x_pC);
     p(:, :, i) = (eye(2) - KG*H)*p_pC; %(eye(2) - KG*H)*p_pC*(eye(2)-KG*H).'+KG*R*KG.';
 end
+end
 
+function plotToGraph(name, x, p, truth, time, TOTAL_POINTS)
+%Plotting Problem 1
 posP = zeros(1,TOTAL_POINTS);
 velP = zeros(1,TOTAL_POINTS); 
 deltaX = zeros(1, TOTAL_POINTS);
 deltaV = zeros(1, TOTAL_POINTS);
+mPEx = zeros(1, TOTAL_POINTS); %mean posistion error distance
+mPEv = zeros(1, TOTAL_POINTS); %mean posistion error velocity
 for i = 1:TOTAL_POINTS
     posP(i) = p(1,1,i);
     velP(i) = p(2,2,i);
     deltaX(i) = x(1,i) - truth(1, i);
     deltaV(i) = x(2,i) - truth(2, i);
+    mPEx(i) = mean(deltaX(1:i));
+    mPEv(i) = mean(deltaV(1:i));
 end
 
-figure(1);
-plotFilter("Posistion", "Difference (m)", posP, time, deltaX);
-figure(2);
-plotFilter("Velocity", "Difference (m/s)", velP, time, deltaV);
+posP = zeros(1,TOTAL_POINTS);
+velP = zeros(1,TOTAL_POINTS); 
+deltaX = zeros(1, TOTAL_POINTS);
+deltaV = zeros(1, TOTAL_POINTS);
+mPEx = zeros(1, TOTAL_POINTS); %mean posistion error distance
+mPEv = zeros(1, TOTAL_POINTS); %mean posistion error velocity
+for i = 1:TOTAL_POINTS
+    posP(i) = p(1,1,i);
+    velP(i) = p(2,2,i);
+    deltaX(i) = x(1,i) - truth(1, i);
+    deltaV(i) = x(2,i) - truth(2, i);
+    mPEx(i) = mean(deltaX(1:i));
+    mPEv(i) = mean(deltaV(1:i));
+end
 
-function plotFilter(name, yl, P, time, delta)
+sgtitle(name);
+subX = 1;
+subY = 2;
+plotFilter("Posistion Error", "Difference (m)", posP, time, deltaX,mPEx, subX,subY,1);
+plotFilter("Velocity Error", "Difference (m/s)", velP, time, deltaV,mPEv, subX,subY,2);
+end
+
+function plotFilter(name, yl, P, time, delta, mPE, subX, subY, pos)
+subplot(subY, subX, pos);
 title(name);
 threeSigmaAbove = 3*sqrt(P);
 hold on
 plot(time, threeSigmaAbove, 'LineWidth',2,  'Color','k');
 plot(time, -threeSigmaAbove, 'LineWidth',2, 'Color','k');
+plot(time, mPE, 'Color', 'r');
 plot(time, delta, 'Color', 'b');
 ylabel(yl);
 xlabel("Time (s)");
