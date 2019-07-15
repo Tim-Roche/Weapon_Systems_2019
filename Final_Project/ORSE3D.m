5f24af6735d397e424ff42da879967600502040a
Tim Roche Laptop <kc2vsa@gmail.com>
2019-07-14 21:50:38 -0400

    Started work on part 2.2

--- Final_Project/ORSE3D.m
+++ Final_Project/ORSE3D.m
@@ -6,7 +6,7 @@ dt = 2;
 TOTAL_POINTS = int64(SIM_TIME/dt) + 1;
 
 %Initial Condition
-P_0 =[0; 0; 400000]; %Initial Posistion
+P_0 = [0; 0; 400000]; %Initial Posistion
 V_0 = [0; 400; 0]; %Initial Velocity
 inits = [P_0; V_0];
 
@@ -15,8 +15,8 @@ carlos = 500;
 
 setAcc = '9.8'; %Truth acceleration is 15m/s^2
 [posP_acc, velP_acc, deltaX_c_acc, deltaV_c_acc, mPEx_acc, mPEv_acc, varX_acc, varV_acc, time] = Kf_MC(setAcc,SIM_TIME,dt,inits,TOTAL_POINTS,carlos);
-graphName_a = "1D ORSE Filter -- a = 15m/s^2";
-figure(2);
+graphName_a = "3D ORSE Filter";
+figure(1);
 movegui('east');
 plotToGraph(graphName_a, posP_acc, velP_acc, deltaX_c_acc, deltaV_c_acc, mPEx_acc, mPEv_acc, varX_acc, varV_acc, time, carlos);
 
@@ -33,7 +33,7 @@ function [posP, velP, deltaX_c, deltaV_c, mPEx, mPEv, varX, varV, time] = Kf_MC(
     for c = 1:carlos
         [x, p, truth, time] = kf(setAcc,SIM_TIME,dt,inits,TOTAL_POINTS);
         for i = 1:TOTAL_POINTS
-            p2x2 = p(:,pull2x2byIndex(i));
+            p2x2 = p(:,pull6x6byIndex(i));
             posP(i) = p2x2(1,1);
             velP(i) = p2x2(2,2);
             deltaX(i) = x(1,i) - truth(1, i);
@@ -60,51 +60,57 @@ providedAcc = str2num(setAcc);
 time = 0:dt:SIM_TIME;
 
 %Defining Key Matrixes
-x = zeros(2, TOTAL_POINTS);
-p = zeros(2,TOTAL_POINTS*2);
+x = zeros(6, TOTAL_POINTS);
 
 %Matrixes
-A = [1, dt; 0, 1]; %Transistion Matrix
+A = eye(6,6) + [0,0,0,dt,0,0;0,0,0,0,dt,0;0,0,0,0,0,dt;0,0,0,0,0,0;0,0,0,0,0,0;0,0,0,0,0,0]; %Transistion Matrix
 
 %"Conversion" Matrix
 H = [1,0];
 
 %ORSE things
 lamda = 9;
-G = [(1/2)*dt^2; dt];
+G = [(1/2)*dt^2;(1/2)*dt^2;(1/2)*dt^2; dt;dt;dt];
 
 %Measurement Errors
 R_true = 900;
 R = 0.95*R_true; 
-
+ 
 %Noise
 W_true = 12;
 W = 0.9*W_true;
 
-M = zeros(2, TOTAL_POINTS*2);
-M(:, pull2x2byIndex(1)) = [1, (1/dt); (1/dt), (2/dt)^2]*R_true; %Process Uncertanty%[R, 0; 0, R];
+M = zeros(6, TOTAL_POINTS*6);
+ROW1 = [1,        (1/dt), (2/dt^2), (3/dt^3), (4/dt^4), (5/dt^5)];
+ROW2 = [  (1/dt), (2/dt^2), (3/dt^3), (4/dt^4), (5/dt^5), (6/dt^6)];
+ROW3 = [(2/dt^2), (3/dt^3), (4/dt^4), (5/dt^5), (6/dt^6), (7/dt^7)];
+ROW4 = [(3/dt^3), (4/dt^4), (5/dt^5), (6/dt^6), (7/dt^7), (8/dt^8)];
+ROW5 = [(4/dt^4), (5/dt^5), (6/dt^6), (7/dt^7), (8/dt^8), (9/dt^9)];
+ROW6 = [(5/dt^5), (6/dt^6), (7/dt^7), (8/dt^8), (9/dt^9), (10/dt^10)];
+M(:, pull6x6byIndex(1)) = [ROW1; ROW2; ROW3; ROW4; ROW5; ROW6 *R_true]; 
 
-D = [0;0];
+D = zeros(6,1);
 
-truth = zeros(2, TOTAL_POINTS);
+truth = zeros(6, TOTAL_POINTS);
 
 previousTruth = inits;
-initalSensorZ = inits(1) + normrnd(0, sqrt(R_true));
+initalSensorZ = inits(1) + normrnd(0, sqrt(R_true), [3,1]);
 for i = 1:TOTAL_POINTS
-    B = [(1/2)*dt^2; dt]; %Control Matrix for accel.
+    B_dis = eye(3)*((1/2)*dt^2);
+    B_vel = eye(3)*dt;
+    B = [B_dis; B_vel]; %Control Matrix for accel.
     
+    accVector = [0;0;-1];
     %Find true posistion
-    acc = providedAcc; 
-    if(DWNA) %Check to see if this is Part 1 or Part 1.1
-        acc = normrnd(0, sqrt(W_true)); %Make Acc white noise instead
-    end
-    truth(:,i) = A*previousTruth + B*acc;
+    acc = providedAcc*accVector; 
+    
+    ACC = [acc; acc];
+    truth(:,i) = A*previousTruth + B*ACC;
     previousTruth = truth(:,i); %Gets around indexing issue when i = 1
     
     %Get New Measured Value
-    noiseX = normrnd(0, sqrt(R_true));
-    measuredX = truth(1,i) + noiseX;%C*Y_m + z; 
-    z = measuredX;
+    noiseX = normrnd(0, sqrt(R_true), [3,1]);
+    z = truth(1,i) + noiseX; %z = measured value not the z dem!!!
 
     if(i == 1)
         x(:, 1) = [z; (z-initalSensorZ)/dt];  
@@ -112,7 +118,7 @@ for i = 1:TOTAL_POINTS
         
     %New Predicted State
     x_pC = A*x(:, i-1);
-    M_mC = A*M(:, pull2x2byIndex(i-1))*A.';
+    M_mC = A*M(:, pull6x6byIndex(i-1))*A.';
     D = A*D + G;
     
     %Gain Computation
@@ -121,7 +127,7 @@ for i = 1:TOTAL_POINTS
     
     %Calculate Current State
     x(:, i) = x_pC + KG*(z - H*x_pC);
-    M(:, pull2x2byIndex(i)) = (eye(2) - KG*H)*M_mC*(eye(2)-KG*H).'+KG*R*KG.';
+    M(:, pull6x6byIndex(i)) = (eye(2) - KG*H)*M_mC*(eye(2)-KG*H).'+KG*R*KG.';
     D = (eye(2) - KG*H)*D;
     end
 end
@@ -161,7 +167,7 @@ hold off
 legend([p4(1), p3, p1, p5],'Orientation', 'Vertical');
 end
 
-function output = pull2x2byIndex(i)
-loc = i*2 - 1;
+function output = pull6x6byIndex(i)
+loc = i*6 - 1;
 output = loc:loc+1;
 end
